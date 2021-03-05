8c366cdcd20a2a894aaff696b50bbde9570a6fa0
DGMan <codelint@foxmail.com>
2021-03-05 15:33:39 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMinor(from: "6.1.1"))
-    ]
+    ],
     targets: [
         .target(name: "LLCycleScrollView", dependencies: ["Kingfisher"])
     ],
