81cab5d42d2c1bb1a58dadcd4baf8774003b6202
DGMan <codelint@foxmail.com>
2021-03-05 16:09:23 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "LLCycleScrollView",
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
     products: [
         .library(name: "LLCycleScrollView", targets: ["LLCycleScrollView"])
     ],
